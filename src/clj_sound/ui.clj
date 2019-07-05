617c6fca59f6fb3ed6464f7fd27d80818c0227dd
A J Henderson <jahenderson777@gmail.com>
2019-07-05 08:08:54 +0100

    cljfx

--- src/clj_sound/ui.clj
+++ src/clj_sound/ui.clj
@@ -6,6 +6,11 @@
 (def *progress
   (atom 0.3))
 
+(def *context
+  (atom
+   (fx/create-context {:x 100
+                       :num-active-nodes 39})))
+
 (defn canvas-progress-bar [{:keys [level width height]}]
   {:fx/type :canvas
    :width width
@@ -42,51 +47,3 @@
 
 (defn launch [*db]
   (fx/mount-renderer *db renderer))
-
-
-
-(comment
-  (defui MyCircle
-    (render [this {:keys [radius]}]
-            (ui/circle
-             :center-x 20
-             :center-y 20
-             :radius (double radius))))
-
-  (defui Stage
-    (render [this db]
-            (ui/stage
-             :title "Hello World!"
-             :shown true
-             :min-width 300
-             :min-height 300
-             :scene (ui/scene
-                     :root (ui/stack-pane
-                            :children [(my-circle db)
-                                       (ui/button
-                                        :text (if (:playing db)
-                                                "Stop"
-                                                "Play")
-                                        :on-action {:event :play-stop})])))))
-
-  (defmulti handle-event (fn [db event] (:event event)))
-
-  (defmethod handle-event :play-stop [db {:keys []}]
-    (update db :playing not))
-
-  (defmethod handle-event :change-color [db {:keys []}]
-    (assoc db :radius (rand-int 200)))
-
-  (defn ui [db]
-    (let [ui-state (agent (dom/app (stage @db)
-                                   #(try
-                                      (swap! db handle-event %)
-                                      (catch Throwable ex
-                                        (println ex)))))]
-      (add-watch db :ui (fn [_ _ _ _]
-                          (send ui-state
-                                #(try
-                                   (dom/update-app % (stage @db))
-                                   (catch Throwable ex
-                                     (println ex))))))))
-  )
